455764aa54a62e59f2e29a72ff9081425047e748
Veselka Marinova <vsl.marinova@gmail.com>
2022-05-17 21:28:47 +0300

    T647: Import Dividend Trading 212.

---
+++ dividend_collection.py
@@ -0,0 +1,29 @@
+from dividend_payment import DividendTransaction
+
+
+class DividendCollection:
+    def __init__(self):
+        self.payments = []
+
+    def append(self, dividend_payment: DividendTransaction):
+        self.payments.append(dividend_payment)
+
+    def is_empty(self):
+        return not self.payments
+
+    def get_security(self):#get_fund_names
+        fund_names = []
+        for payment in self.payments:
+            if not payment.fund_name in fund_names:
+                fund_names.append(payment.fund_name)
+        return fund_names
+
+    def get_fund_payments(self, fund_name):
+        fund_payments = []
+        for payment in self.payments:
+            if payment.fund_name == fund_name:
+                fund_payments.append(payment)
+        return fund_payments
+
+    def sort_by_date(self, reverse):
+        self.payments.sort(key=lambda x: x.date_time, reverse=reverse)
---
+++ importers/trading212/dividends_importer.py
@@ -0,0 +1,54 @@
+from dividend_payment import DividendTransaction
+import csv
+import decimal
+
+FUND_DOMICILE_MAPPING = {
+    "IWQU": "Ireland",
+    "IUSN": "Ireland",
+    "IQQW": "Ireland",
+    "VAGF": "Ireland",
+    "IS3S": "Ireland",
+    "IQQH": "Ireland",
+}
+
+FUND_NAME_MAPPING = {
+    "IWQU": "iShares Edge MSCI World Quality Factor",
+    "IUSN": "iShares MSCI World Small Cap",
+    "IQQW": "iShares MSCI World EUR",
+    "VAGF": "Vanguard Global Aggregate Bond",
+    "IS3S": "iShares Edge MSCI World Value Factor",
+    "IQQH": "iShares Global Clean Energy",
+}
+
+
+class Trading212DividendsImporter:
+    @staticmethod
+    def import_dividends(collection, file_name):
+        with open(file_name, newline='') as csvfile:
+            csvreader = csv.reader(csvfile)
+            # We skip the first line, because it shows column headers
+            next(csvreader)
+            for row in csvreader:
+
+                # If we don't have the fund this row is a bank transaction. Skip it!
+                security = row[4]
+                if not security:
+                    continue
+
+                # The transaction list should only contain buys and sells. Skip dividends.
+                if row[0] != "Dividend (Ordinary)":
+                    continue
+                company = security.split(' ')[0]
+                dividend = decimal.Decimal(row[10])
+                currency = row[7]
+                purchase_price = decimal.Decimal(row[11])
+                print(purchase_price)
+                ticker = row[3]
+                country = FUND_DOMICILE_MAPPING.get(ticker)
+                tax_paid = row[11]
+                if not country:
+                    print("Error could not find domicile " + ticker + " add it to FUND_DOMICILE_MAPPING!")
+                    exit()
+                dividend_payment = DividendTransaction(security, company, dividend, country, tax_paid, currency, purchase_price)
+                collection.append(dividend_payment)
+
