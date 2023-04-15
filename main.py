d7d1059a3a72ad48b953a7aea1feba7691c3bbd6
Veselka Marinova <vsl.marinova@gmail.com>
2023-04-15 19:15:14 +0300

    Code clean up.

--- main.py
+++ main.py
@@ -14,11 +14,28 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, PatternFill
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--binck-transactions', '--binck', dest='transactions_binck', help='The path to an excel file detailing BinckBank transactions.')
-    parser.add_argument('--trading212-transactions', '--t212', dest='transactions_trading212', help='The path to an csv file detailing Trading 212 transactions + dividends.', action='append', nargs='*')
-    parser.add_argument('--saxo-transactions', '--saxo', dest='transactions_saxo', help='The path to an excel file detailing Saxo transactions.')
+    parser.add_argument(
+        "--binck-transactions",
+        "--binck",
+        dest="transactions_binck",
+        help="The path to an excel file detailing BinckBank transactions.",
+    )
+    parser.add_argument(
+        "--trading212-transactions",
+        "--t212",
+        dest="transactions_trading212",
+        help="The path to an csv file detailing Trading 212 transactions + dividends.",
+        action="append",
+        nargs="*",
+    )
+    parser.add_argument(
+        "--saxo-transactions",
+        "--saxo",
+        dest="transactions_saxo",
+        help="The path to an excel file detailing Saxo transactions.",
+    )
     args = parser.parse_args()
 
     # If the user doesn't supply input print the help and exit.
@@ -34,11 +51,15 @@ if __name__ == '__main__':
         for csv_files in args.transactions_trading212:
             # Argparse allows to add multiple values for an option in a row.
             for csv_file in csv_files:
-                Trading212TransactionsImporter.import_transactions(transactions, csv_file)
+                Trading212TransactionsImporter.import_transactions(
+                    transactions, csv_file
+                )
                 Trading212DividendsImporter.import_dividends(payments, csv_file)
 
     if args.transactions_binck:
-        BinckTransactionsImporter.import_transactions(transactions, args.transactions_binck)
+        BinckTransactionsImporter.import_transactions(
+            transactions, args.transactions_binck
+        )
 
     if args.transactions_saxo:
         SaxoImporter.import_transactions(transactions, args.transactions_saxo)
@@ -52,14 +73,14 @@ if __name__ == '__main__':
         ws = wb.active
 
         # Create the header row.
-        ws['A1'] = "Fund name"
-        ws['B1'] = "Domicile"
-        ws['C1'] = "Transaction date"
-        ws['D1'] = "Transaction type"
-        ws['E1'] = "Number"
-        ws['F1'] = "Share price"
-        ws['G1'] = "Total shares"
-        ws['H1'] = "Purchase price"
+        ws["A1"] = "Fund name"
+        ws["B1"] = "Domicile"
+        ws["C1"] = "Transaction date"
+        ws["D1"] = "Transaction type"
+        ws["E1"] = "Number"
+        ws["F1"] = "Share price"
+        ws["G1"] = "Total shares"
+        ws["H1"] = "Purchase price"
 
         # Set a blue background color and white font color on row 1 (the header row).
         for cell in ws[1]:
@@ -67,14 +88,14 @@ if __name__ == '__main__':
             cell.font = Font(name="Calibri", color="FFFFFF")
 
         # Column widths.
-        ws.column_dimensions['A'].width = 40
-        ws.column_dimensions['B'].width = 10
-        ws.column_dimensions['C'].width = 20
-        ws.column_dimensions['D'].width = 20
-        ws.column_dimensions['E'].width = 15
-        ws.column_dimensions['F'].width = 15
-        ws.column_dimensions['G'].width = 15
-        ws.column_dimensions['H'].width = 15
+        ws.column_dimensions["A"].width = 40
+        ws.column_dimensions["B"].width = 10
+        ws.column_dimensions["C"].width = 20
+        ws.column_dimensions["D"].width = 20
+        ws.column_dimensions["E"].width = 15
+        ws.column_dimensions["F"].width = 15
+        ws.column_dimensions["G"].width = 15
+        ws.column_dimensions["H"].width = 15
 
         transactions.calc_total_shares()
         transactions.sort_by_date(True)
@@ -84,13 +105,24 @@ if __name__ == '__main__':
 
             # Add each transaction as a new row in the excel sheet.
             for transaction in fund_transactions:
-                ws.append([transaction.fund_name, transaction.domicile, transaction.date_time, transaction.transaction_type, transaction.number, transaction.share_price, transaction.total, transaction.purchase_price])
+                ws.append(
+                    [
+                        transaction.fund_name,
+                        transaction.domicile,
+                        transaction.date_time,
+                        transaction.transaction_type,
+                        transaction.number,
+                        transaction.share_price,
+                        transaction.total,
+                        transaction.purchase_price,
+                    ]
+                )
 
                 # Format the date as DD.MM.YYYY.
                 ws.cell(ws.max_row, 3).number_format = "DD.MM.YYYY"
 
                 # Format negative number in number column.
-                number_format = '#,0.000;[RED]-#,0.000'
+                number_format = "#,0.000;[RED]-#,0.000"
                 # Format and styling the negative number in number column.
                 ws.cell(ws.max_row, 5).number_format = number_format
                 # Format and styling total share column.
@@ -98,7 +130,13 @@ if __name__ == '__main__':
 
                 # Format the currencies.
                 # See https://support.microsoft.com/en-us/office/number-format-codes-5026bbd6-04bc-48cd-bf33-80f18b4eae68?ui=en-us&rs=en-us&ad=us
-                number_format = '#,##0.00 [$' + transaction.currency + '];[RED]-#,##0.00 [$' + transaction.currency + ']'
+                number_format = (
+                    "#,##0.00 [$"
+                    + transaction.currency
+                    + "];[RED]-#,##0.00 [$"
+                    + transaction.currency
+                    + "]"
+                )
                 # Format the share price as a currency.
                 ws.cell(ws.max_row, 6).number_format = number_format
                 # Format the purchase price as a currency.
@@ -110,7 +148,6 @@ if __name__ == '__main__':
         # Save the file
         wb.save("investments.xlsx")
         print("Saved file investments.xlsx!")
-
 
         # Dividend payments
         if not payments.is_empty():
@@ -183,4 +220,3 @@ if __name__ == '__main__':
             # Save the file
             wb.save("dividends.xlsx")
             print("Saved file dividends.xlsx!")
-
